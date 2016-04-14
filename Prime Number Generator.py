f4aaad761c8512ec6dcdffba11bebfc240cbfd43
cjrichardson <cjrichardson76@gmail.com>
2016-04-14 21:13:39 +0100

    Changed sleep to 10 seconds

--- Prime Number Generator.py
+++ Prime Number Generator.py
@@ -5,7 +5,7 @@ import time
 Primes = input("Please Enter how many numbers you would like to check are prime, starting from 2 to anything!: ")
 Message = ("Ok - Let me calculate all prime numbers from 2 to %s. Give me a few seconds....i'm slow!")
 print(Message %(Primes))
-time.sleep (5)
+time.sleep (10)
 for i in range (2,int(Primes)+1):
 	j = 2
 	counter = 0
